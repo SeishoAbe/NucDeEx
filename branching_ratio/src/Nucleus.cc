adab93d04de114d50ba9d1c2d934ff36790f272f
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-05-24 04:25:06 +0000

    delete array at the destructer

--- branching_ratio/src/Nucleus.cc
+++ branching_ratio/src/Nucleus.cc
@@ -77,4 +77,47 @@ nucleus::nucleus(const char* name, int z, int n){
 }
 
 
+nucleus::~nucleus(){
+	delete[] pop;
+	delete[] Ex;
 
+	delete[] Ex_bin_g;
+	delete[] Ex_bin_n;
+	delete[] Ex_bin_p;
+	delete[] Ex_bin_a;
+	delete[] Ex_bin_d;
+	delete[] Ex_bin_t;
+	delete[] Ex_bin_h;
+
+  for(int i=0;i<array;i++){
+		delete[] pop_g[i];
+		delete[] pop_n[i];
+		delete[] pop_p[i];
+		delete[] pop_a[i];
+		delete[] pop_d[i];
+		delete[] pop_t[i];
+		delete[] pop_h[i];
+		delete[] Ex_g[i];
+		delete[] Ex_n[i];
+		delete[] Ex_p[i];
+		delete[] Ex_a[i];
+		delete[] Ex_d[i];
+		delete[] Ex_t[i];
+		delete[] Ex_h[i];
+	}
+
+	delete[] pop_g;
+	delete[] pop_n;
+	delete[] pop_p;
+	delete[] pop_a;
+	delete[] pop_d;
+	delete[] pop_t;
+	delete[] pop_h;
+	delete[] Ex_g;
+	delete[] Ex_n;
+	delete[] Ex_p;
+	delete[] Ex_a;
+	delete[] Ex_d;
+	delete[] Ex_t;
+	delete[] Ex_h;
+}
