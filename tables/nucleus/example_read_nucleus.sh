7da377e2240317b84fe1e7e2ca72238ab35b1f09
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-06 03:31:41 +0000

    modify example shell script for reading nucleus table

---
+++ tables/nucleus/example_read_nucleus.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILE=$TALYS_WORK_TABLES/nucleus/nucleus.txt
+
+cat $FILE | while read line
+do 
+	check=${line:0:1}
+	if [ $check = "#" ] ; then
+		continue
+	fi
+	nucleus=`echo $line |cut -d ' ' -f 1`
+	Z=`echo $line |cut -d ' ' -f 2`
+	N=`echo $line |cut -d ' ' -f 3`
+	maxlevelsbin=`echo $line |cut -d ' ' -f 4`
+	echo $nucleus $Z $N $maxlevelsbin
+done
