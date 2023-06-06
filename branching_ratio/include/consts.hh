4878b9c3e4232d5a4a685d91d37bf31d26ec6bd9
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-06 02:05:58 +0000

    change separation E table format. change particle array order (alpha goes to the last)g

--- branching_ratio/include/consts.hh
+++ branching_ratio/include/consts.hh
@@ -9,6 +9,19 @@ static const int bins=100;
 static const int parity=2;
 static const int num_particle=7;
 static const string particle_name[num_particle]
+	= {"gamma","neutron","proton",
+		 "deuteron", "triton","helium-3","alpha"};
+static const int color_root[num_particle]
+	= {880+1, // violet+1
+		 600, // bule
+		 632, // red
+		 920+1, // gray +1
+		 432+1, // cyan+1
+		 416+1,// green+1
+		 616 // magenta
+		};
+		/*
+static const string particle_name[num_particle]
 	= {"gamma","neutron","proton","alpha",
 		 "deuteron", "triton","helium-3"};
 static const int color_root[num_particle]
@@ -21,5 +34,6 @@ static const int color_root[num_particle]
 		 416+1 // green+1
 		};
 
+*/
 static const float check_criteria=0.02;
 #endif
