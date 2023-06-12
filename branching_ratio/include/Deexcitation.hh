d5a3ce7018ead8635fc26c37b4e11712afda12e3
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-12 13:10:55 +0000

    Determine sequential decay mode until zero Ex. Get mass using ROOT libraries. Kinematics are not calculated yet.

--- branching_ratio/include/Deexcitation.hh
+++ branching_ratio/include/Deexcitation.hh
@@ -5,10 +5,15 @@
 #include <ostream>
 #include "NucleusTable.hh"
 #include "consts.hh"
+
 #include <TFile.h>
 #include <TTree.h>
 #include <TGraph.h>
 #include <TRandom3.h>
+#include <TDatabasePDG.h>
+#include <TParticle.h>
+#include <TGeoManager.h>
+#include <TGeoElement.h>
 
 using namespace std;
 
@@ -24,30 +29,51 @@ class Deexcitation{
 	void SetVerbose(int v){ verbose=v; };
 
 	private:
-	// Simulation method called by DoDeex()
+	// --- Simulation method called by DoDeex()
 	int DecayMode(double Ex);
 	int NearestExPoint(double Ex, int decay_mode);
 	bool DaughterExPoint(double &d_Ex, int &d_point);
+	vector<TParticle*> Decay();
+	const char* PDGion(int Z,int N);
 
-	// ROOT related methods & members
-	bool ReadROOT(const char* name);
+	// --- ROOT related methods & members
+	bool OpenROOT(const char* name);
+	bool GetBrTGraph();
 	TFile* rootf;
 	TTree* tree;
 	TGraph* g_br[num_particle];
 	TGraph* g_br_ex;
+	TRandom3* rndm;
+	TDatabasePDG* pdg;
+	TGeoManager* geo;
+	TGeoElementTable* element_table;
 
-	// Target info
+	double ElementMassInMeV(TGeoElementRN* ele);
+
+	// -- Nucleus info
+	// target info
 	int Z_target, N_target;
 	double Ex_target;
+	Nucleus* nuc_target;
+	double mass_target;
+	TGeoElementRN* e_target;
 	string name_target;
 
-	// nucleus table
-	NucleusTable* _nucleus_table;
+	// daughter info
+	int Z_daughter, N_daughter;
+	double Ex_daughter;
+	Nucleus* nuc_daughter;
+	double mass_daughter;
+	TGeoElementRN* e_daughter;
 
-	// Random generator
-	TRandom3* rndm;
+	// particle info
+	double mass_particle;
+	double S;
+	double Qvalue;
+
 
 	// others
+	NucleusTable* _nucleus_table;
 	int verbose;
 	ostringstream os;
 };
--- branching_ratio/src/Deexcitation.cc
+++ branching_ratio/src/Deexcitation.cc
@@ -13,6 +13,9 @@
 #include "consts.hh"
 #include <TGraph.h>
 #include <TRandom3.h>
+#include <TParticlePDG.h>
+#include <TParticle.h>
+#include <TGeoPhysicalConstants.h>
 
 using namespace std;
 
@@ -27,51 +30,97 @@ Deexcitation::Deexcitation()
 		abort();
 	}
 	verbose=0;
-	rndm = new TRandom3(0);
+	rndm = new TRandom3(0); // 0: seed is from time
+	pdg = new TDatabasePDG();
+	geo = new TGeoManager("test","test");
+	element_table = geo->GetElementTable();
 }
 
 ///////////////////////////
 Deexcitation::~Deexcitation()
 ///////////////////////////
 {
-	delete rndm;
 	delete _nucleus_table;
+	delete rndm;
+	delete pdg;
+	delete geo;
 }
 
 /////////////////////////////////////////////
 void Deexcitation::DoDeex(int Z,int N, double Ex)
 /////////////////////////////////////////////
 {
-	cout << "Deexcitation::DoDeex(" << Z << "," << N << "," << Ex << ")" << endl;
+	cout << endl << "###################################" << endl;
+	cout << "Deexcitation::DoDeex(" << Z << "," << N << "," 
+			 << Ex << ")" << endl;
+	cout << "###################################" << endl;
 	
 	// store target info. we don't want to change original value...
-	Z_target=Z;
-	N_target=N;
-	Ex_target=Ex;
-	name_target = (string)_nucleus_table->GetNucleusPtr(Z_target,N_target)->name;
+	Z_target   = Z;
+	N_target   = N;
+	Ex_target  = Ex;
+	nuc_target = _nucleus_table->GetNucleusPtr(Z_target,N_target);
+	name_target = (string)nuc_target->name;
 
 	// Read ROOT file
-	if( ! ReadROOT(name_target.c_str()) ){
+	if( ! OpenROOT(name_target.c_str()) ){
 		cout << "We don't have deexcitation profile for this nucleus: " 
 				 << name_target.c_str() << endl;
 		return;
 	}
-
-	//while(Ex_target>0){
+	
+	// loop until zero excitation energy
+	// Use parameters named as "_target"
+	while(Ex_target>0){
+		cout << endl << "### " << name_target << ",   Ex = " << Ex_target << endl;
+		GetBrTGraph(); // get TGraph based on name_target
 		
 		// --- Determine decay mode 
+		//     Return: The same as array in consts.hh
+		//     It also sets Z_daughter, Z_daughter
 		int decay_mode = DecayMode(Ex_target); 
 
 		// --- Get nearest Ex bin (TGraph point) & get (TGraph*) br_ex
-		int nearest_ex_point = NearestExPoint(Ex_target,decay_mode); 
+		int Ex_target_point = NearestExPoint(Ex_target,decay_mode); 
 
 		// --- Determine daughter excitation energy
-		int daughter_ex_point;
-		double daughter_ex;
-		DaughterExPoint(daughter_ex,daughter_ex_point);
+		int Ex_daughter_point;
+		DaughterExPoint(Ex_daughter,Ex_daughter_point);
 
+		// get mass info using ROOT libraries
+		if(decay_mode<=2){ // obtained from TDatabasePDG
+			mass_particle = pdg->GetParticle(PDG_particle[decay_mode])->Mass()/TGeoUnit::MeV;//GeV2MeV
+		}else{ // obtained from TGeoElementRN
+			int a_particle = (PDG_particle[decay_mode]%1000)/10;
+			int z_particle = ((PDG_particle[decay_mode]%1000000)-a_particle*10)/10000;
+			TGeoElementRN* element_rn =	element_table->GetElementRN(a_particle,z_particle); // (A,Z)
+			cout << "a_particle = " << a_particle << "   z_particle = " << z_particle << endl;
+			mass_particle = ElementMassInMeV(element_rn);
+		}
+		mass_target = ElementMassInMeV(element_table->GetElementRN(Z_target+N_target, Z_target));
+		mass_daughter = ElementMassInMeV(element_table->GetElementRN(Z_daughter+N_daughter, Z_daughter));
 
-	//}
+		// get separation E & Qvalue 
+		S = nuc_target->S[decay_mode];
+		Qvalue = Ex_target - S - Ex_daughter;
+		if(Qvalue<0) Qvalue=0;
+		if(verbose>0){
+			cout << "S = " << S << endl;
+			cout << "Qvalue = " << Qvalue << endl;
+		}
+
+		Decay();
+
+		// end of while loop: daughter -> target 
+		if(nuc_daughter==NULL) break;
+		Ex_target = Ex_daughter;
+		Z_target  = Z_daughter;
+		N_target  = N_daughter;
+		nuc_target = _nucleus_table->GetNucleusPtr(Z_target,N_target);
+		name_target = (string)nuc_target->name;
+	}
+		
+
 
 	rootf->Close();
 	delete rootf;
@@ -109,8 +158,32 @@ int Deexcitation::DecayMode(double Ex)
 			break; 
 		} 
 	}
-	if(verbose>0){
-		cout << "Random = " << random << " --> " << particle_name[decay_mode] << endl;
+
+	// --- Set Z and N for daughter 
+	Z_daughter = Z_target;
+	N_daughter = N_target;
+	if(decay_mode==1){
+		N_daughter--;
+	}else if(decay_mode==2){
+		Z_daughter--;
+	}else if(decay_mode==3){
+		Z_daughter--;
+		N_daughter--;
+	}else if(decay_mode==4){
+		Z_daughter--;
+		N_daughter-=2;
+	}else if(decay_mode==5){
+		Z_daughter-=2;
+		N_daughter--;
+	}else if(decay_mode==6){
+		Z_daughter-=2;
+		N_daughter-=2;
+	}
+	nuc_daughter = _nucleus_table->GetNucleusPtr(Z_daughter,N_daughter);
+
+	if(verbose>0 && nuc_daughter!=NULL){
+		cout << "Random = " << random << " : " << name_target.c_str() << " --> " << particle_name[decay_mode]
+				  << " + " << nuc_daughter->name << endl;
 	}
 
 	return decay_mode;
@@ -161,7 +234,7 @@ bool Deexcitation::DaughterExPoint(double &d_Ex, int &d_point)
 		if(Br_integ>random) break;
 	}
 	if(verbose>0){
-		cout << "Random = " << random << " --> ex = " << ex
+		cout << "Random = " << random << " : ex = " << ex
 		     << ",   point = " << point << endl;
 	}
 
@@ -171,38 +244,68 @@ bool Deexcitation::DaughterExPoint(double &d_Ex, int &d_point)
 	return 1;
 }
 
+/////////////////////////////////////////////
+vector<TParticle*> Deexcitation::Decay()
+/////////////////////////////////////////////
+{
+	cout << "Deexcitation::Decay()" << endl;
+
+	cout << "mass_target (MeV)   = " << mass_target << endl;
+	cout << "mass_particle (MeV) = " << mass_particle << endl;
+	cout << "mass_daughter (MeV) = " << mass_daughter << endl;
+	// HOGEHOGE //
 
 
+	vector<TParticle*> particle;
+	return particle;
+}
+
+/////////////////////////////////////////////
+double Deexcitation::ElementMassInMeV(TGeoElementRN* ele)
+/////////////////////////////////////////////
+{
+	double mass = ele->MassNo()*(TGeoUnit::amu_c2/TGeoUnit::MeV)
+									 + ele->MassEx();
+	double mass_amu = mass/(TGeoUnit::amu_c2/TGeoUnit::MeV);
+	if(verbose>0){
+		cout << "mass (MeV) = " << mass 
+				 << "    (amu) = " << mass_amu << endl;
+	}
+	return mass;
+}
 
 
-
-
-
-
-
-
+/////////////////////////////////////////////
+const char* Deexcitation::PDGion(int Z, int N)
+/////////////////////////////////////////////
+{
+	os.str("");
+	os << "10" << setw(3) << setfill('0') << Z
+						 << setw(3) << setfill('0') << Z+N << "0";
+	return os.str().c_str();
+}
 
 
 
 /////////////////////////////////////////////
-bool Deexcitation::ReadROOT(const char* name)
+bool Deexcitation::OpenROOT(const char* name)
 /////////////////////////////////////////////
 {
 	os.str("");
 	os << getenv("TALYS_WORK_BR") << "/output/Br_" << name << ".root"; 
 	rootf = new TFile(os.str().c_str(),"READ");
 	if(! rootf->IsOpen()) return 0;
+	return 1;
+}
+
+/////////////////////////////////////////////
+bool Deexcitation::GetBrTGraph()
+/////////////////////////////////////////////
+{
 	for(int p=0;p<num_particle;p++){
 		os.str("");
-		os << "g_" << name << "_br_" << p;
+		os << "g_" << name_target.c_str() << "_br_" << p;
 		g_br[p] = (TGraph*) rootf->Get(os.str().c_str());
 	}
-	/*
-	tree = (TTree*) rootf->Get("tree");
-	tree->SetBranchAddress("name",&name);
-	tree->SetBranchAddress("Z",&Z);
-	tree->SetBranchAddress("N",&N);
-	tree->SetBranchAddress("Ex_bin",&Ex_bin);
-	*/
 	return 1;
 }
