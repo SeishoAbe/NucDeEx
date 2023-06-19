6b1f927c6924fdf0440b16195ef59c4ce2020c12
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-19 11:07:05 +0000

    this plot tools is only for 11B

---
+++ plot_simulation_11B.C
@@ -0,0 +1,509 @@
+#include "include/consts.hh"
+#include <TStyle.h>
+#include <map>
+
+using namespace std;
+
+const double kE_th[num_particle]
+	= {0,3.1,3.1,
+		 4.0,4.6,1e9,4.5};
+
+int plot_simulation_comparison(){
+	// ---- FIXME ---- //
+	string target = "11B";
+	const double Ex_min =16;
+	const double Ex_max =35;
+		// negative -> not applied
+	const bool flag_decay=1;
+		// 0 -> use string w/ "g" (gamma)
+		// 1- > use string w/o "g" (gamma) <- use this 
+	// --------------- //
+
+	ostringstream os;
+	os.str("");
+	os << "sim_out/" << target.c_str() << ".root";
+	TFile* rootf = new TFile(os.str().c_str(),"READ");
+	cout << os.str().c_str() << endl;
+	TTree* tree = (TTree*) rootf->Get("tree");
+	int eventID, size;
+	double MissE, Ex, S;
+	double PinitMag, PinitX,PinitY,PinitZ;
+	//
+	int PDG[bins];
+	double mass[bins];
+	double totalE[bins],kE[bins];
+	double PMag[bins], PX[bins],PY[bins],PZ[bins];
+	string* decay=0;
+	tree->SetBranchAddress("eventID",&eventID);
+	if(flag_decay) tree->SetBranchAddress("decay_remove_g",&decay);
+	else           tree->SetBranchAddress("decay",&decay);
+	tree->SetBranchAddress("MissE",&MissE);
+	tree->SetBranchAddress("S",&S);
+	tree->SetBranchAddress("Ex",&Ex);
+	tree->SetBranchAddress("PinitMag",&PinitMag);
+	tree->SetBranchAddress("PinitX",&PinitX);
+	tree->SetBranchAddress("PinitY",&PinitY);
+	tree->SetBranchAddress("PinitZ",&PinitZ);
+	tree->SetBranchAddress("size",&size);
+	tree->SetBranchAddress("PDG",&PDG);
+	tree->SetBranchAddress("mass",&mass);
+	tree->SetBranchAddress("totalE",&totalE);
+	tree->SetBranchAddress("kE",&kE);
+	tree->SetBranchAddress("PMag",&PMag);
+	tree->SetBranchAddress("PX",&PX);
+	tree->SetBranchAddress("PY",&PY);
+	tree->SetBranchAddress("PZ",&PZ);
+
+	// --- Draw --- // 
+	gStyle->SetTextFont(132);
+	gStyle->SetTextSize(0.08);
+	gStyle->SetTitleSize(0.05,"XYZ");
+	gStyle->SetTitleFont(132,"XYZ");
+	gStyle->SetLabelSize(0.05,"XYZ");
+	gStyle->SetLabelFont(132,"XYZ");
+	gStyle->SetLegendFont(132);
+	gStyle->SetTitleYOffset(0.95);
+	
+	TH1D* h_Ex = new TH1D("h_Ex","",500,-100,400);
+	TH1D* h_nmulti = new TH1D("h_nmulti","",10,-0.5,9.5);
+	TH1D* h_kE[num_particle];
+	TH1D* h_Ex_particle[num_particle], *h_Ex_particle_th[num_particle]; // w/ experimental threshold
+	for(int p=0;p<num_particle;p++){
+		os.str("");
+		os << "h_kE_" << p;
+		if(p==0) h_kE[p]= new TH1D(os.str().c_str(),"",200,0,20);
+		else     h_kE[p]= new TH1D(os.str().c_str(),"",40,0,20);
+		//
+		os.str("");
+		os << "h_Ex_particle_" << p;
+		h_Ex_particle[p] = new TH1D(os.str().c_str(),"",500,-100,400);
+		h_Ex_particle[p]->SetLineColor(color_root[p]);
+		//h_Ex_particle[p]->SetFillColor(color_root[p]);
+		//
+		os.str("");
+		os << "h_Ex_particle_th_" << p;
+		h_Ex_particle_th[p] = new TH1D(os.str().c_str(),"",500,-100,400);
+		h_Ex_particle_th[p]->SetLineColor(color_root[p]);
+		//h_Ex_particle_th[p]->SetFillColor(color_root[p]);
+	}
+
+	int max_size=0;
+	int numofevent=0, numofdetected=0;
+	map<string, double> br;
+	map<string, double> :: iterator itr;
+	double rbr[num_particle]={0}; // two-body
+	double rbr_2b[num_particle]={0}; // two-body
+	for(int i=0;i<tree->GetEntries();i++){
+		tree->GetEntry(i);
+
+		// --- Ex selection --- //
+		h_Ex->Fill(Ex);
+		if(Ex_min>0 && Ex<Ex_min) continue;
+		if(Ex_max>0 && Ex>Ex_max) continue;
+		numofevent++;
+
+		if(max_size<size) max_size=size;
+
+		int nmulti=0;
+		int particle_counter=0; // w/o g
+		bool flag_detected[num_particle]={0};
+		for(int b=0;b<size;b++){
+			if(PDG[b]==2112) nmulti++;
+			int p=-1;
+			for(int par=0;par<num_particle;par++){
+				if(PDG_particle[par]==PDG[b]){
+					p=par;
+					break;
+				}
+			}
+			if(p>=0){ // this is particle info (not daughter nuclei)
+				h_kE[p]->Fill(kE[b]);
+				h_Ex_particle[p]->Fill(Ex);
+				if(kE[b]<kE_th[p]) continue;
+				flag_detected[p] = 1; // detected
+				if(p!=0) particle_counter++;
+			}
+		}
+		h_nmulti->Fill(nmulti); // w/o energy th
+
+		bool detected=0;
+		for(int par=1;par<num_particle;par++){
+			//cout << flag_detected[par] << "  ";
+			if(!flag_detected[par]) continue;
+			rbr[par]++;
+			if(particle_counter==1) rbr_2b[par]++;
+			h_Ex_particle_th[par]->Fill(Ex);
+			detected=1;
+		}
+		//cout << endl;
+		if(detected) numofdetected++;
+
+
+		// save map
+		itr = br.find(decay->c_str());
+		if(itr != end(br) ) {
+			itr->second += 1;
+		}else{
+			//cout << decay->c_str() << endl;
+			br.insert(make_pair(decay->c_str(),1));
+		}
+	}
+	cout << "max_size=" << max_size << endl;
+	cout << "numofevent=" << numofevent << endl;
+	cout << "numofdetected=" << numofdetected << endl;
+
+	double rbr_sum=0, rbr_2b_sum=0;
+	for(int par=0;par<num_particle;par++){
+		rbr[par] = rbr[par]/numofdetected*100; // %
+		rbr_2b[par] = rbr_2b[par]/numofdetected*100; // %
+		cout << setw(10) << particle_name[par].c_str() << "  " 
+				 << setw(10) << rbr[par] << "  " << rbr_2b[par] << endl;
+		rbr_sum+=rbr[par];
+		rbr_2b_sum+=rbr_2b[par];
+	}
+	cout << setw(10) << "sum" << "  " << setw(10) << rbr_sum << "  " << rbr_2b_sum << endl;
+
+	// for comparison 
+	// nda relative br (2body)
+	double rbr_nda_n = rbr_2b[1]/(rbr_2b[1]+rbr_2b[3]+rbr_2b[6]);
+	double rbr_nda_da = (rbr_2b[3]+rbr_2b[6])/(rbr_2b[1]+rbr_2b[3]+rbr_2b[6]);
+	cout << "Relative BR (n vs d/a): n   : " << rbr_nda_n << endl;
+	cout << "Relative BR (n vs d/a): d/a : " << rbr_nda_da << endl;
+
+	
+	// output  (txt)
+	os.str("");
+	if(Ex_min>0) os << "_Exmin" << Ex_min;
+	if(Ex_max>0) os	<< "_Exmax" << Ex_max;
+	string suffix = os.str();
+	os.str("");
+	os << "sim_out/" << target.c_str() << suffix.c_str() << "_raw.txt";
+	ofstream ofs_raw (os.str().c_str());
+	os.str("");
+	os << "sim_out/" << target.c_str() << suffix.c_str() << ".txt";
+	ofstream ofs (os.str().c_str());
+	const double br_th = 0.5;
+	ofs << "# br threshold written in this table: " << br_th << endl;
+	double br_sum=0; // for check
+	double br_others=0;
+	for(itr = br.begin();itr!=br.end();itr++){
+		double br_s = itr->second/numofevent*100;
+		br_sum += br_s;
+		ofs_raw  << setw(10) << itr->first << "   " 
+				 << setw(10) << fixed << setprecision(2) << br_s << " (%)" << endl;
+		if(br_s>br_th){
+			ofs  << setw(10) << itr->first << "   " 
+					 << setw(10) << fixed << setprecision(2) << br_s << " (%)" << endl;
+		}else br_others+=br_s;
+	}
+	cout << "(check) br_sum = " << br_sum << " (%)" << endl;
+	ofs_raw.close();
+	ofs  << setw(10) << "others" << "   " 
+			 << setw(10) << fixed << setprecision(2) << br_others << " (%)" << endl;
+	ofs.close();
+
+	// --- Scale histograms 
+	h_Ex->Scale(1./h_Ex->GetEntries());
+	h_nmulti->Scale(1./h_nmulti->GetEntries());
+	for(int p=0;p<num_particle;p++){
+		h_kE[p]->Scale(1./h_kE[p]->GetEntries());
+		h_Ex_particle[p]->Scale(1.0/h_Ex->GetEntries());
+		h_Ex_particle_th[p]->Scale(1.0/h_Ex->GetEntries());
+	}
+
+
+	TCanvas* c_Ex = new TCanvas("c_Ex","",0,0,800,600);
+	os.str("");
+	os << "fig_sim/fig_" << target.c_str() << "_Ex" << suffix.c_str() << ".pdf";
+	string pdfname=os.str();
+	c_Ex->Print( (pdfname+"[").c_str());
+	c_Ex->Update();
+	c_Ex->Clear();
+	//
+	h_Ex->GetXaxis()->SetRangeUser(0,100);
+	h_Ex->SetStats(0);
+	h_Ex->SetMinimum(0);
+	h_Ex->GetXaxis()->SetTitle("Excitation energy (MeV)");
+	h_Ex->GetYaxis()->SetTitle("A.U.");
+	h_Ex->Draw("HIST");
+	TLine* l_1_Ex_min = new TLine(Ex_min,0,Ex_min,h_Ex->GetMaximum()*1.05);
+	l_1_Ex_min->SetLineWidth(2);
+	l_1_Ex_min->SetLineStyle(2);
+	l_1_Ex_min->SetLineColor(kRed);
+	if(Ex_min>8) l_1_Ex_min->Draw("same");
+	TLine* l_1_Ex_max = new TLine(Ex_max,0,Ex_max,h_Ex->GetMaximum()*1.05);
+	l_1_Ex_max->SetLineWidth(2);
+	l_1_Ex_max->SetLineStyle(2);
+	l_1_Ex_max->SetLineColor(kRed);
+	if(Ex_max>8) l_1_Ex_max->Draw("same");
+	os.str("");
+	c_Ex->Print(pdfname.c_str());
+	c_Ex->Update();
+	c_Ex->Clear();
+	//
+	h_Ex->SetTitle("w/o experimental energy threshold");
+	h_Ex->GetXaxis()->SetRangeUser(8,47);
+	h_Ex->Draw("HIST");
+	for(int p=1;p<num_particle;p++){
+		h_Ex_particle[p]->Draw("HISTsame");
+	}
+	TLine* l_Ex_min = new TLine(Ex_min,0,Ex_min,h_Ex->GetMaximum()*1.05);
+	l_Ex_min->SetLineWidth(2);
+	l_Ex_min->SetLineStyle(2);
+	l_Ex_min->SetLineColor(kRed);
+	if(Ex_min>8 && Ex_min<47) l_Ex_min->Draw("same");
+	TLine* l_Ex_max = new TLine(Ex_max,0,Ex_max,h_Ex->GetMaximum()*1.05);
+	l_Ex_max->SetLineWidth(2);
+	l_Ex_max->SetLineStyle(2);
+	l_Ex_max->SetLineColor(kRed);
+	if(Ex_max>8 && Ex_max<47) l_Ex_max->Draw("same");
+	os.str("");
+	os << "fig_sim/fig_" << target.c_str() << "_Ex" << suffix.c_str() << ".pdf";
+	c_Ex->Print(os.str().c_str());
+	c_Ex->Update();
+	c_Ex->Clear();
+	//
+	THStack* h_s_Ex = new THStack("h_s_Ex","");
+	h_Ex->Draw("HIST");
+	for(int p=2;p<num_particle;p++){
+		h_Ex_particle[p]->SetFillColor(color_root[p]);
+		h_s_Ex->Add(h_Ex_particle[p]);
+	}
+	h_s_Ex->Draw("HISTsame");
+	c_Ex->Print(pdfname.c_str());
+	c_Ex->Update();
+	c_Ex->Clear();
+	//
+	h_Ex->SetTitle("w/ experimental energy threshold");
+	h_Ex->GetXaxis()->SetRangeUser(8,47);
+	h_Ex->Draw("HIST");
+	for(int p=0;p<num_particle;p++){
+		h_Ex_particle_th[p]->Draw("HISTsame");
+	}
+	if(Ex_max>8 && Ex_max<47) l_Ex_max->Draw("same");
+	if(Ex_min>8 && Ex_min<47) l_Ex_min->Draw("same");
+	os.str("");
+	os << "fig_sim/fig_" << target.c_str() << "_Ex" << suffix.c_str() << ".pdf";
+	c_Ex->Print(os.str().c_str());
+	c_Ex->Update();
+	c_Ex->Clear();
+	//
+	THStack* h_s_Ex_th = new THStack("h_s_Ex_th","");
+	h_Ex->Draw("HIST");
+	for(int p=2;p<num_particle;p++){
+		h_Ex_particle_th[p]->SetFillColor(color_root[p]);
+		h_s_Ex_th->Add(h_Ex_particle_th[p]);
+	}
+	h_s_Ex_th->Draw("HISTsame");
+	c_Ex->Print(pdfname.c_str());
+	c_Ex->Update();
+	c_Ex->Clear();
+	c_Ex->Print((pdfname+"]").c_str());
+	c_Ex->Update();
+	c_Ex->Clear();
+
+
+	TCanvas* c_nmulti = new TCanvas("c_nmulti","",0,0,800,600);
+	h_nmulti->GetXaxis()->SetNdivisions(10);
+	h_nmulti->GetXaxis()->SetTitle("Neutron multiplicity");
+	h_nmulti->GetYaxis()->SetTitle("Branching ratio");
+	h_nmulti->SetStats(0);
+	h_nmulti->Draw("HIST");
+	os.str("");
+	os << "Mean = " << setprecision(3) << h_nmulti->GetMean();
+	TText* t_nmulti = new TText(5,h_nmulti->GetMaximum()*0.5,os.str().c_str());
+	t_nmulti->Draw("same");
+	//
+	os.str("");
+	os << "fig_sim/fig_" << target.c_str() << "_nmulti" << suffix.c_str() << ".pdf";
+	c_nmulti->Print(os.str().c_str());
+
+	
+	TCanvas* c_kE = new TCanvas("c_kE","",0,0,1200,600);
+	c_kE->Divide(4,2);
+	for(int p=0;p<num_particle;p++){
+		c_kE->cd(p+1);
+		h_kE[p]->SetTitle(particle_name[p].c_str());
+		h_kE[p]->SetStats(0);
+		h_kE[p]->GetXaxis()->SetTitle("Kinetic energy (MeV)");
+		h_kE[p]->GetYaxis()->SetTitle("A.U.");
+		h_kE[p]->SetLineColor(color_root[p]);
+		if(p!=0) h_kE[p]->SetLineWidth(2);
+		h_kE[p]->Draw("HIST");
+		os.str("");
+		os << "Prob = " << fixed << (double)h_kE[p]->GetEntries()/numofevent;
+		TText* t_kE = new TText(7,h_kE[p]->GetMaximum()*0.7,os.str().c_str());
+		t_kE->Draw("same");
+	}
+	c_kE->cd(8);
+	TPaveText* t = new TPaveText(0.1,0.1,0.9,0.9);
+	os.str("");
+	os << "# generated events = " << numofevent;
+	t->AddText(os.str().c_str());
+	t->Draw("same");
+	//
+	os.str("");
+	os << "fig_sim/fig_" << target.c_str() << "_kE" << suffix.c_str() << ".pdf";
+	c_kE->Print(os.str().c_str());
+
+
+	//----------------------------//
+	// nda plot
+	const int nda_data=3;
+	const int nda_color[nda_data]={416+2,800+6,600-6};
+	const int	nda_color_this = 616-6;
+	string nda_data_name[nda_data]={"SAbe_Tohoku_2023","Hu","Panin"};
+	string nda_data_legend[nda_data]={"Abe (2023)","Hu et al. (2022)","Panin et al. (2016)"};
+	TFile* rootf_nda[nda_data];
+	TH1D* h_nda[nda_data];
+	for(int i=0;i<nda_data;i++){
+		os.str("");
+		os << "data/" << nda_data_name[i].c_str() << "/" << nda_data_name[i].c_str() << ".root";
+		rootf_nda[i] = new TFile(os.str().c_str(),"READ");
+		TEnv* env = (TEnv*) rootf_nda[i]->Get("env");
+		//
+		os.str("");
+		os << "h_nda_" << nda_data_name[i].c_str();
+		h_nda[i] = new TH1D(os.str().c_str(),"",20,-10,10);
+		h_nda[i]->Fill(i,env->GetValue("rbr_nda_n",-9999.));
+		h_nda[i]->Fill(i+nda_data+3,env->GetValue("rbr_nda_da",-9999.));
+		h_nda[i]->SetFillColor(nda_color[i]);
+	}
+	TH1D* h_nda_this = new TH1D("h_nda_this","",20,-10,10);
+	h_nda_this->Fill(-1,rbr_nda_n*1e2);
+	h_nda_this->Fill(-1+nda_data+3,rbr_nda_da*1e2);
+	h_nda_this->SetFillColor(nda_color_this);
+
+
+	TCanvas* c_nda =new TCanvas("c_nda","c_nda",0,0,800,600);
+	TH1F* waku_nda = gPad->DrawFrame(-2,0,10,85);
+	waku_nda->GetXaxis()->SetLabelSize(0);
+	waku_nda->GetXaxis()->SetTickSize(0);
+	waku_nda->GetYaxis()->SetTitle("Relative branching ratio (%)");
+	for(int i=0;i<nda_data;i++){
+		h_nda[i]->Draw("HISTsame");
+	}
+	h_nda_this->Draw("HISTsame");
+	TLegend* leg_nda = new TLegend(0.6,0.6,0.9,0.89);
+	leg_nda->SetBorderSize(0);
+	leg_nda->SetFillStyle(0);
+	leg_nda->AddEntry(h_nda_this,"This work","F");
+	for(int i=0;i<nda_data;i++){
+		os.str("");
+		os << nda_data_legend[i].c_str();
+		leg_nda->AddEntry(h_nda[i],os.str().c_str(),"F");
+	}
+	leg_nda->Draw("same");
+	TLatex* l_n = new TLatex(1,-8,"n");
+	l_n->SetTextFont(12);
+	l_n->Draw("same");
+	TLatex* l_da = new TLatex(0.5+nda_data+3,-8,"d/#alpha");
+	l_da->SetTextFont(12);
+	l_da->Draw("same");
+	//
+	os.str("");
+	os << "fig_sim/fig_" << target.c_str() << "_nda" << suffix.c_str() << ".pdf";
+	c_nda->Print(os.str().c_str());
+
+
+
+	//---  br plot
+	const int br_data=4;
+	const int br_color[br_data]={416+2,800+6,600,1};
+	const int	br_color_this = 616-6;
+	string br_data_name[br_data]={"SAbe_Tohoku_2023","Hu","CASCADE","Yosoi"};
+	string br_data_legend[br_data]={"Abe (2023)","Hu et al. (2022)","CASCADE (Yosoi et al.)", "Yosoi et al. (exp.)"};
+	TFile* rootf_br[br_data];
+	TH1D* h_br[br_data];
+	TH1D* h_br_2b[br_data];
+	for(int i=0;i<br_data;i++){
+		os.str("");
+		os << "data/" << br_data_name[i].c_str() << "/" << br_data_name[i].c_str() << ".root";
+		if(br_data_name[i]=="CASCADE"){
+			os.str("");
+			os << "data/Yosoi/Yosoi.root";
+		}
+		cout << os.str().c_str() << endl;
+		rootf_br[i] = new TFile(os.str().c_str(),"READ");
+		TEnv* env = (TEnv*) rootf_br[i]->Get("env");
+		//
+		os.str("");
+		os << "h_br_" << br_data_name[i].c_str();
+		h_br[i] = new TH1D(os.str().c_str(),"",50,-10,40);
+		os.str("");
+		os << "h_br_2b_" << br_data_name[i].c_str();
+		h_br_2b[i] = new TH1D(os.str().c_str(),"",50,-10,40);
+		//
+		if(br_data_name[i]=="CASCADE"){
+			h_br[i]->Fill(i,env->GetValue("cas_br_n",-9999.));
+			h_br_2b[i]->Fill(i,env->GetValue("cas_br_n_2b",-9999.));
+			h_br[i]->Fill(i+br_data*1.5,env->GetValue("cas_br_p",-9999.));
+			h_br_2b[i]->Fill(i+br_data*1.5,env->GetValue("cas_br_p_2b",-9999.));
+			h_br[i]->Fill(i+br_data*3,env->GetValue("cas_br_d",-9999.));
+			h_br_2b[i]->Fill(i+br_data*3,env->GetValue("cas_br_d_2b",-9999.));
+			h_br[i]->Fill(i+br_data*4.5,env->GetValue("cas_br_t",-9999.));
+			h_br_2b[i]->Fill(i+br_data*4.5,env->GetValue("cas_br_t_2b",-9999.));
+			h_br[i]->Fill(i+br_data*6,env->GetValue("cas_br_a",-9999.));
+			h_br_2b[i]->Fill(i+br_data*6,env->GetValue("cas_br_a_2b",-9999.));
+		}else{
+			h_br[i]->Fill(i,env->GetValue("br_n",-9999.));
+			h_br_2b[i]->Fill(i,env->GetValue("br_n_2b",-9999.));
+			h_br[i]->Fill(i+br_data*1.5,env->GetValue("br_p",-9999.));
+			h_br_2b[i]->Fill(i+br_data*1.5,env->GetValue("br_p_2b",-9999.));
+			h_br[i]->Fill(i+br_data*3,env->GetValue("br_d",-9999.));
+			h_br_2b[i]->Fill(i+br_data*3,env->GetValue("br_d_2b",-9999.));
+			h_br[i]->Fill(i+br_data*4.5,env->GetValue("br_t",-9999.));
+			h_br_2b[i]->Fill(i+br_data*4.5,env->GetValue("br_t_2b",-9999.));
+			h_br[i]->Fill(i+br_data*6,env->GetValue("br_a",-9999.));
+			h_br_2b[i]->Fill(i+br_data*6,env->GetValue("br_a_2b",-9999.));
+		}
+		//
+		h_br[i]->SetLineColor(br_color[i]);
+		h_br_2b[i]->SetFillStyle(3445);
+		h_br_2b[i]->SetLineColor(br_color[i]);
+		h_br_2b[i]->SetFillColor(br_color[i]);
+	}
+
+	TH1D* h_br_this  = new TH1D("h_br_this","",50,-10,40);
+	TH1D* h_br_2b_this  = new TH1D("h_br_2b_this","",50,-10,40);
+	h_br_this->SetLineColor(br_color_this);
+	h_br_2b_this->SetFillStyle(3445);
+	h_br_2b_this->SetLineColor(br_color_this);
+	h_br_2b_this->SetFillColor(br_color_this);
+	//
+	h_br_this->Fill(-1,rbr[1]/2);
+	h_br_2b_this->Fill(-1,rbr_2b[1]/2);
+	h_br_this->Fill(-1+br_data*1.5,rbr[2]);
+	h_br_2b_this->Fill(-1+br_data*1.5,rbr_2b[2]);
+	h_br_this->Fill(-1+br_data*3,rbr[3]);
+	h_br_2b_this->Fill(-1+br_data*3,rbr_2b[3]);
+	h_br_this->Fill(-1+br_data*4.5,rbr[4]);
+	h_br_2b_this->Fill(-1+br_data*4.5,rbr_2b[4]);
+	h_br_this->Fill(-1+br_data*6,rbr[6]);
+	h_br_2b_this->Fill(-1+br_data*6,rbr_2b[6]);
+
+
+
+	TCanvas* c_br =new TCanvas("c_br","c_br",0,0,800,600);
+	TH1F* waku_br = gPad->DrawFrame(-3,0,30,32);
+	waku_br->GetXaxis()->SetLabelSize(0);
+	waku_br->GetXaxis()->SetTickSize(0);
+	waku_br->GetYaxis()->SetTitle("Relative branching ratio (%)");
+	h_br_this->Draw("HISTsame");
+	h_br_2b_this->Draw("HISTsame");
+	TLegend* leg_br = new TLegend(0.6,0.6,0.9,0.9);
+	leg_br->SetBorderSize(0);
+	leg_br->SetFillStyle(0);
+	leg_br->AddEntry(h_br_this,"This work","l");
+	for(int i=0;i<br_data;i++){
+		h_br[i]->Draw("HISTsame");
+		h_br_2b[i]->Draw("HISTsame");
+		os.str("");
+		os << br_data_legend[i].c_str();
+		leg_br->AddEntry(h_br[i],os.str().c_str(),"l");
+	}
+	leg_br->Draw("same");
+
+
+	return 0;
+}
