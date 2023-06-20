dac37523ca1834fdf8c1156310aa2e773818809a
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-20 08:43:14 +0000

    add ldmodel flag to talys shell and plotter

--- run_plot_decay.sh
+++ run_plot_decay.sh
@@ -4,11 +4,15 @@
 #TARGET_TMP="11B 11C 15N 15O"
 #TARGET_TMP="15O 15N"
 TARGET_TMP="11B"
+ldmodel_tmp="1 2 3 4 5 6"
 #############
 
 for TARGET in $TARGET_TMP
 do
-	mkdir fig/$TARGET
-	mkdir output/$TARGET
-	./bin/plot_decay $TARGET
+	for ldmodel in $ldmodel_tmp
+	do
+		mkdir fig/${TARGET}_ldmodel${ldmodel}
+		mkdir output/${TARGET}_ldmodel${ldmodel}
+		./bin/plot_decay $TARGET $ldmodel
+	done
 done
