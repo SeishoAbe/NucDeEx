891dbd643861c5230e45d826b1b83d4061a230e6
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-21 08:40:19 +0000

    add parity optmodall flag to simulator

--- run_simulation.sh
+++ run_simulation.sh
@@ -2,7 +2,8 @@
 
 ### FIXME ###
 TARGET_TMP="11B"
-ldmodel_tmp="1 2 3 4 5 6"
+ldmodel_tmp="1 2 3"
+parity_optmodall_tmp="0 1"
 #############
 
 LogDir=log
@@ -11,7 +12,13 @@ for TARGET in $TARGET_TMP
 do
 	for ldmodel in $ldmodel_tmp
 	do
-		LogFile=$LogDir/log_${TARGET}_ldmodel${ldmodel}
-		./bin/simulation $TARGET $ldmodel > $LogFile 2>&1 &
+		for parity_optmodall in $parity_optmodall_tmp
+		do
+			LogFile=$LogDir/log_${TARGET}_ldmodel${ldmodel}
+			if [ $parity_optmodall -eq 1 ] ; then
+				LogFile+="_parity_optmodall"
+			fi
+			./bin/simulation $TARGET $ldmodel $parity_optmodall > $LogFile 2>&1 &
+		done
 	done
 done
