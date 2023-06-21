2214656f5349022d17ce680a46879cfc4582dd72
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-21 06:42:06 +0000

    add optmodall & parity flag to talys plotter

--- run_plot_decay.sh
+++ run_plot_decay.sh
@@ -4,15 +4,22 @@
 #TARGET_TMP="11B 11C 15N 15O"
 #TARGET_TMP="15O 15N"
 TARGET_TMP="11B 11C"
-ldmodel_tmp="1 2 3 4 5 6"
+ldmodel_tmp="1 2 3"
+parity_optmodall_tmp="0 1"
 #############
 
 for TARGET in $TARGET_TMP
 do
 	for ldmodel in $ldmodel_tmp
 	do
-		mkdir fig/${TARGET}_ldmodel${ldmodel}
-		#mkdir output/${TARGET}_ldmodel${ldmodel}
-		./bin/plot_decay $TARGET $ldmodel
+		for parity_optmodall in $parity_optmodall_tmp
+		do
+			FIGDIR=fig/${TARGET}_ldmodel${ldmodel}
+			if [ $parity_optmodall -eq 1 ] ; then
+				FIGDIR+="_parity_optmodall"
+			fi
+			mkdir $FIGDIR
+			./bin/plot_decay $TARGET $ldmodel $parity_optmodall
+		done
 	done
 done
