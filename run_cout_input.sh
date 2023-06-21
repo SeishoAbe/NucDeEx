13e91ed39f0d210f87052d4c319b2ef572070e10
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-21 06:42:06 +0000

    add optmodall & parity flag to talys plotter

--- run_cout_input.sh
+++ run_cout_input.sh
@@ -3,6 +3,8 @@
 ### FIXME ###
 TARGET_TMP="11B 11C 15N 15O"
 ldmodel_tmp="1 2 3 4 5 6"
+parity_tmp="0 1"
+optmodall_tmp="0 1"
 #############
 
 EXE=./bin/cout_input
@@ -11,6 +13,12 @@ for TARGET in $TARGET_TMP
 do
 	for ldmodel in $ldmodel_tmp
 	do
-		$EXE $TARGET $ldmodel
+		for parity in $parity_tmp
+		do
+			for optmodall in $optmodall_tmp
+			do
+					$EXE $TARGET $ldmodel $parity $optmodall
+			done
+		done
 	done
 done
--- run_talys.sh
+++ run_talys.sh
@@ -2,10 +2,15 @@
 
 ### FIXME ###
 #TARGET_TMP="11B 11C 15N 15O"
-TARGET_TMP="11C 11B"
+#TARGET_TMP="11B"
+TARGET_TMP="11C"
 FILE_TMP="energy.1.2.p.txt energy"
-ldmodel_tmp="1 2 3 4 5 6"
+ldmodel_tmp="1 2 3"
+parity_optmodall_tmp="0 1"
 #############
+
+#ldmodel_tmp="1 2 3 4 5 6"
+# 4-6 (microscopic is not available):w
 
 OUTDIR=output
 INDIR=input
@@ -14,24 +19,32 @@ cd /home/seisho/talys_1.96
 . setup_talys_1.96.sh
 cd -
 
-
-if [ ! -e $OUTDIR ] ; then
-  echo "not output dir"
-  exit
-else 
-  cd $OUTDIR
-fi
-
-for FILE in $FILE_TMP
-do
-	ln -sv $TALYS_WORK_TABLES/energy_distribution/$FILE ./
-done
-
 for TARGET in $TARGET_TMP
 do
+	OUTDIR=$OUTDIR/$TARGET
+	if [ ! -e $OUTDIR ] ; then
+		mkdir $OUTDIR
+	fi
+	cd $OUTDIR
+
+	for FILE in $FILE_TMP
+	do
+		ln -sv $TALYS_WORK_TABLES/energy_distribution/$FILE ./
+	done
+
 	for ldmodel in $ldmodel_tmp
 	do
-		talys < ../$INDIR/input_${TARGET}_ldmodel${ldmodel} > output_${TARGET}_ldmodel${ldmodel}
+		for parity_optmodall in $parity_optmodall_tmp
+		do
+			INFILE=$TALYS_WORK/$INDIR/input_${TARGET}_ldmodel${ldmodel}
+			OUTFILE=output_${TARGET}_ldmodel${ldmodel}
+			if [ $parity_optmodall -eq 1 ] ; then
+				INFILE+="_parity_optmodall"
+				OUTFILE+="_parity_optmodall"
+			fi
+
+			talys < $INFILE > $OUTFILE
+		done
 	done
+	cd -
 done
-cd -
