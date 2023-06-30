b32554f02cb206cc52189f99c19fa6f7abf5dfd9
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-06-30 11:37:38 +0000

    add deexcitation process for p3/2

--- include/Deexcitation.hh
+++ include/Deexcitation.hh
@@ -24,8 +24,40 @@ class Deexcitation{
 	Deexcitation(const int ldmodel=1, const bool parity_optmodall=1);
 	virtual ~Deexcitation();
 
-	void DoDeex(const int Z, const int N, const double Ex,
-							const TVector3& mom=TVector3(0,0,0));
+	// --- Main function ---//
+	int DoDeex(const int Zt, const int Nt,
+		 				 const int Z, const int N, const int shell, const double Ex,
+						 const TVector3& mom=TVector3(0,0,0));
+	// Zt, Nt  : Target nucleus Z and N (supports 16O or 12C currently)
+	// Z, N    : Residual nucleus Z and N (having excitation energy)
+	// shell 1 : s1/2-hole
+	//			 2 : p3/2-hole
+	//       3 : p1/2-hole (only for 16O target)
+	// Ex      : Only used if shell==1
+	// mom     : 3D momentum of residual nucleus
+	// 
+	// return  0 : The target or residual nucleus is not supported
+	//         1 : Success
+	//        -1 : Fatal error
+
+	// ---Sub functions --- //
+	// supports multi-nucleon disapperance
+	int DoDeex_talys(const int Zt, const int Nt,
+							     const int Z, const int N, const double Ex,
+							     const TVector3& mom=TVector3(0,0,0));
+	// return 0: No talys tables. Nothing to do.
+	//				1: Sucess
+	//       -1: Very rarely happens due to production of 5H etc. (no mass table)
+	//           If you find this, please execute this again
+
+	// only for single-nucleon disappearance
+	int DoDeex_p32(const int Zt, const int Nt,
+								 const int Z, const int N,
+							   const TVector3& mom=TVector3(0,0,0));
+	// Note: Energy level is determined irrelevant to Ex
+	// return  1: Sucess
+	//        -1 : Fatal error
+
 
 	void SetSeed(int s){ rndm->SetSeed(s) ;};
 	int  GetSeed(){return rndm->GetSeed();};
@@ -95,5 +127,29 @@ class Deexcitation{
 	int eventID;
 	ostringstream os;
 	const double check_criteria=1e-3;
+
+	// Constants for (p3/2)-1 Br
+	// 11B* (Panin et al., Phys. Lett. B 753 204-210. Experimental data)
+	static const int Nlevel_p32_11B = 3; 
+	const double E_p32_11B[Nlevel_p32_11B]={0.,2.125,5.020};
+	const double Br_p32_11B[Nlevel_p32_11B]={0.82,0.10,0.08};
+	// 11C* (assume analogy of 11B*)
+	//      the same Br, but energy is different
+	static const int Nlevel_p32_11C = 3;
+	const double E_p32_11C[Nlevel_p32_11C]={0.,2.000,4.804};
+	const double Br_p32_11C[Nlevel_p32_11C]={0.82,0.10,0.08};
+
+	// 15N* (Ejili, Phys. Rev. C 58, 3)
+	//		 Deexcitation from 9.93 MeV will be described from TALYS data
+	//     Deexcitation from 10.70 is taken from Ejiri (100% proton)
+	static const int Nlevel_p32_15N = 3;
+	const double E_p32_15N[Nlevel_p32_15N]={6.32,9.93,10.70};
+	const double Br_p32_15N[Nlevel_p32_15N]={0.872,0.064,0.064};
+	// 15O* (Ejiri , Phys. Rev. C 58, 3)
+	static const int Nlevel_p32_15O=1;
+	const double E_p32_15O[Nlevel_p32_15O]={6.18};
+	const double Br_p32_15O[Nlevel_p32_15O]={1.};
+
+
 };
 #endif
