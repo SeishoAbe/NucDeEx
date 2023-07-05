07b137b2e308bfb02f1a248d9bfc7342824911b2
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-07-05 08:10:08 +0000

    modify shells for multi-nucleon hole :

--- run_plot_decay.sh
+++ run_plot_decay.sh
@@ -2,11 +2,17 @@
 
 ### FIXME ###
 #TARGET_TMP="11B 11C 15N 15O"
-TARGET_TMP="15O 15N"
-#TARGET_TMP="11B 11C"
-ldmodel_tmp="1 2 3"
-parity_optmodall_tmp="0 1"
+#ldmodel_tmp="1 2 3"
+#parity_optmodall_tmp="0 1"
+#flag_jpi=1
+
+TARGET_TMP="14O 13O 12O 11O 14N 13N 12N 11N 10N 14C 13C 12C 11C 10C 9C 13B 12B 11B 10B 9B 8B 12Be 11Be 10Be 9Be 8Be 7Be 11Li 10Li 9Li 8Li 7Li 6Li"
+ldmodel_tmp=2
+parity_optmodall_tmp=1
+flag_jpi=0
 #############
+
+LogDir=log
 
 for TARGET in $TARGET_TMP
 do
@@ -14,12 +20,28 @@ do
 	do
 		for parity_optmodall in $parity_optmodall_tmp
 		do
-			FIGDIR=fig/${TARGET}_ldmodel${ldmodel}
-			if [ $parity_optmodall -eq 1 ] ; then
-				FIGDIR+="_parity_optmodall"
+			FIGDIR=fig
+			if [ $flag_jpi -eq 1 ] ; then
+				if [ $TARGET = 11C ] || [ $TARGET = 11B ] ; then
+					FIGDIR+=/12C
+				elif [ $TARGET = 15N ] || [ $TARGET = 15O ] ; then
+					FIGDIR+=/16O
+				else 
+					continue
+				fi
 			fi
 			mkdir $FIGDIR
-			./bin/plot_decay $TARGET $ldmodel $parity_optmodall
+			FIGDIR+=/${TARGET}_ldmodel${ldmodel}
+			LogFile=$LogDir/log_plot_decay_${TARGET}_ldmodel${ldmodel}
+			if [ $parity_optmodall -eq 1 ] ; then
+				FIGDIR+="_parity_optmodall"
+				LogFile+="_parity_optmodall"
+			fi
+			if [ $flag_jpi -eq 1 ] ; then 
+				LogFile+="_flag_jpi"
+			fi
+			mkdir $FIGDIR
+			./bin/plot_decay $TARGET $ldmodel $parity_optmodall $flag_jpi > $LogFile 2>&1 &
 		done
 	done
 done
--- run_talys.sh
+++ run_talys.sh
@@ -4,16 +4,19 @@
 #TARGET_TMP="11B 11C 15N 15O"
 #ldmodel_tmp="1 2 3"
 #parity_optmodall_tmp="0 1"
-TARGET_TMP="15N"
-ldmodel_tmp="1"
-parity_optmodall_tmp="0"
+#flag_jpi=1
+
+TARGET_TMP="14O 13O 12O 11O 14N 13N 12N 11N 10N 14C 13C 12C 11C 10C 9C 13B 12B 11B 10B 9B 8B 12Be 11Be 10Be 9Be 8Be 7Be 11Li 10Li 9Li 8Li 7Li 6Li"
+ldmodel_tmp=2
+parity_optmodall_tmp=1
+flag_jpi=0
 #############
-FILE_TMP="energy.1.2.p.txt energy"
 
-#ldmodel_tmp="1 2 3 4 5 6"
-# 4-6 (microscopic is not available)
+FILE_TMP="energy.1.2.p energy"
+#ldmodel_tmp="1 2 3 4 5 6" # 4-6 (microscopic is not available)
 
-INDIR=input
+INDIR_PREFIX=input
+OUTDIR_PREFIX=output
 
 cd /home/seisho/talys_1.96
 . setup_talys_1.96.sh
@@ -21,7 +24,20 @@ cd -
 
 for TARGET in $TARGET_TMP
 do
-	OUTDIR=output/$TARGET
+	INDIR=$INDIR_PREFIX
+	OUTDIR=$OUTDIR_PREFIX
+	if [ $flag_jpi -eq 1 ] ; then
+		if [ $TARGET = 11C ] || [ $TARGET = 11B ] ; then
+			INDIR+=/12C
+			OUTDIR+=/12C
+		elif [ $TARGET = 15N ] || [ $TARGET = 15O ] ; then
+			INDIR+=/16O
+			OUTDIR+=/16O
+		else 
+			continue
+		fi
+	fi
+
 	if [ ! -e $OUTDIR ] ; then
 		mkdir $OUTDIR
 	fi
@@ -42,7 +58,6 @@ do
 				INFILE+="_parity_optmodall"
 				OUTFILE+="_parity_optmodall"
 			fi
-
 			talys < $INFILE > $OUTFILE
 		done
 	done
