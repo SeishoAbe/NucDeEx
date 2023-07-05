6e5483b4d1ee15738a59a08175da913e281d050b
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-07-05 08:02:24 +0000

    add tree to output. array->tgraph. save both

--- main/plot_decay.cc
+++ main/plot_decay.cc
@@ -87,13 +87,39 @@ int main(int argc, char* argv[]){
 	os << ".root";
 	TFile* rootf = new TFile(os.str().c_str(),"RECREATE");
 	TTree* tree = new TTree("tree","tree");
-	int Z,N, Ex_bin;
 	string name;
+	int Z,N;
+	int Ex_bin[num_particle];
+	float Pop[parity][bins];
+	float Ex[num_particle][bins];
+	float Br[num_particle][bins];
+	int REx_bin[num_particle][bins];
+	float REx[num_particle][bins][bins], RBr[num_particle][bins][bins];
 	tree->Branch("name",&name);
 	tree->Branch("Z",&Z,"Z/I");
 	tree->Branch("N",&N,"N/I");
-	tree->Branch("Ex_bin",&Ex_bin,"Ex_bin/I");
-
+	os.str("");
+	os << "Pop[" << parity << "][" << bins << "]/F";
+	tree->Branch("Pop",&Pop,os.str().c_str());
+	os.str("");
+	os << "Ex_bin[" << num_particle << "]/I";
+	tree->Branch("Ex_bin",&Ex_bin,os.str().c_str());
+	os.str("");
+	os << "Ex[" << num_particle << "][" << bins << "]/F";
+	tree->Branch("Ex",&Ex,os.str().c_str());
+	os.str("");
+	os << "Br[" << num_particle << "][" << bins << "]/F";
+	tree->Branch("Br",&Br,os.str().c_str());
+	//
+	os.str("");
+	os << "REx_bin[" << num_particle << "][" << bins << "]/I";
+	tree->Branch("REx_bin",&REx_bin,os.str().c_str());
+	os.str("");
+	os << "REx[" << num_particle << "][" << bins << "][" << bins << "]/F";
+	tree->Branch("REx",&REx,os.str().c_str());
+	os.str("");
+	os << "RBr[" << num_particle << "][" << bins << "][" << bins << "]/F";
+	tree->Branch("RBr",&RBr,os.str().c_str());
 
 	// start Nucleus loop in NucleusTable
 	for(int inuc=0;inuc<num_of_nuc;inuc++){
@@ -112,31 +138,127 @@ int main(int argc, char* argv[]){
 			cerr << "ERROR: There was unexpected behaviour in Ex" << endl;
 			return 0;
 		}
-		int bin_target = nuc_target->Ex_bin[0];
+
+		// Init array
+		for(int p=0;p<num_particle;p++){
+			Ex_bin[p]=0;
+			for(int b=0;b<bins;b++){
+				Ex[p][b]=Br[p][b]=0;
+				REx_bin[p][b]=0;
+				for(int b1=0;b1<bins;b1++){
+					REx[p][b][b1]=RBr[p][b][b1]=0;
+				}
+			}
+		}
 
 		// for ttree
 		name = (string)nuc_target->name;
 		Z = nuc_target->Z;
 		N = nuc_target->N;
-		Ex_bin=bin_target;
+		int bin_target = nuc_target->Ex_bin[0];
+
+		int index[parity]={0};
+		int	index_br[num_particle]={0};
+		int index_br_ex[num_particle][bins]={0};
+		float max_total_pop[parity]={0};
+		for(int par=0;par<parity;par++){
+			index[par]=0;
+			max_total_pop[par]=0;
+		}
+		for(int p=0;p<num_particle;p++){
+			index_br[p] = 0;
+			for(int i=0;i<bins;i++){ // target bin loop
+				index_br_ex[p][i]=0;
+			}
+		}
+
+		for(int i=0;i<bin_target;i++){ // target bin loop
+			float Ex_target = nuc_target->Ex[0][i];
+			float pop_target_sum = nuc_target->GetPopParitySum(i);
+
+			for(int par=0;par<parity;par++){
+				float pop_target_norm = nuc_target->pop[par][i]/nuc_target->total_pop[par];
+				// fill normalized population
+				//g_target_pop[par]->SetPoint(index[par],Ex_target,pop_target_norm);
+				Pop[par][index[par]] = pop_target_norm;
+				if(max_total_pop[par]<pop_target_norm) max_total_pop[par] = pop_target_norm;
+				index[par]++;
+			}
+
+			// calculate br
+			for(int p=0;p<num_particle;p++){ // particle loop
+				if(!nuc_target->flag_decay_data[i] && i<=20){ // no decay data
+					if(p==0){
+						//g_target_br[p]->SetPoint(index_br[p],Ex_target,1);
+						Ex[p][index_br[p]] = Ex_target;
+						Br[p][index_br[p]] = 1;
+						// Fill level info only for gamma
+						for(int li=0;li<=i;li++){
+							//g_target_br_ex[p][i]->SetPoint(index_br_ex[p][i],	
+							//															 nuc_target->level_Ex[li],
+							//															 nuc_target->level_br[i][li]*1e-2); // % to frac
+							REx[p][index_br[p]][index_br_ex[p][index_br[p]]] = nuc_target->level_Ex[li];
+							RBr[p][index_br[p]][index_br_ex[p][index_br[p]]] = nuc_target->level_br[i][li]*1e-2;
+							index_br_ex[p][index_br[p]]++;
+						}
+						index_br[p]++;
+					}else{
+						//g_target_br[p]->SetPoint(index_br[p],Ex_target,0);
+						Ex[p][index_br[p]] = Ex_target;
+						Br[p][index_br[p]] = 0;
+						index_br[p]++;
+					}
+				}else if(pop_target_sum>0 && nuc_target->flag_decay_data[i]){ // have decay data
+					float population = nuc_target->GetPopDaughterBinSum(p,i);//  (particle, exbin)
+					float br = population/pop_target_sum;
+					if(br<=0) br=0;
+					if(br>=1) br=1;
+					//g_target_br[p]->SetPoint(index_br[p],Ex_target,br);
+					Ex[p][index_br[p]] = Ex_target;
+					Br[p][index_br[p]] = br;
+					for(int j=0;j<nuc_target->Ex_bin_p[p][i] && population>0;j++){ // daughter ex bin loop
+						float br_ex = nuc_target->pop_p[p][i][j]/population;
+						if(br_ex<=0) br_ex=0;
+						if(br_ex>=1) br_ex=1;
+						//g_target_br_ex[p][i]->SetPoint(index_br_ex[p][i],
+						//															 nuc_target->Ex_p[p][i][j],br_ex);
+						REx[p][index_br[p]][index_br_ex[p][index_br[p]]] = nuc_target->Ex_p[p][i][j];
+						RBr[p][index_br[p]][index_br_ex[p][index_br[p]]] = br_ex;
+						index_br_ex[p][index_br[p]]++;
+					}
+					index_br[p]++;
+				}else{
+					cerr << "Warning: No decay data, but have population " << endl;
+					cerr << "pop_targe_sum = " << pop_target_sum << endl;
+					cerr << "decay_data = " << nuc_target->flag_decay_data[i] << endl;
+					cerr << "bin = " << i << endl;
+					cerr << "particle = " << p << endl;
+				}
+			} // end of p (particle) loop
+		}// end of i (target bin) loop
+
+		for(int p=0;p<num_particle;p++){ // particle loop
+			Ex_bin[p]       = index_br[p];
+			for(int i=0;i<index_br[p];i++){ // target bin loop
+				REx_bin[p][i]  = index_br_ex[p][i];
+			}
+		}
+
+		// --- Fill Tree --- //
 		tree->Fill();
 
-		// Prepare TGraph
-		// population TGraph
+		// --- Prepare TGraph --- //
 		TGraph* g_target_pop[parity];
-		int index[parity]={0};
+		TGraph* g_target_br[num_particle];
+		TGraph* g_target_br_ex[num_particle][bin_target];
 		for(int par=0;par<parity;par++){
-			g_target_pop[par] = new TGraph;
+			g_target_pop[par] = new TGraph(index[par],Ex[0],Pop[par]);
 			os.str("");
 			os << "g_" << name.c_str() << "_pop_" << par;
 			g_target_pop[par]->SetName(os.str().c_str());
 		}
-		// target br TGraph
-		TGraph* g_target_br[num_particle];
-		int	index_br[num_particle]={0};
 		for(int p=0;p<num_particle;p++){
-			index_br[p]=0;
-			g_target_br[p] = new TGraph();
+			g_target_br[p] = new TGraph(Ex_bin[p],Ex[p],Br[p]);
 			os.str("");
 			os << "g_" << name.c_str() << "_br_" << p;
 			g_target_br[p]->SetName(os.str().c_str());
@@ -145,70 +267,16 @@ int main(int argc, char* argv[]){
 			g_target_br[p]->SetLineWidth(2);
 			g_target_br[p]->SetLineColor(color_root[p]);
 		}
-		// target br & ex of daugther
-		TGraph* g_target_br_ex[num_particle][bin_target];
-		int index_br_ex[num_particle][bin_target]={0};
 		for(int p=0;p<num_particle;p++){
 			for(int i=0;i<bin_target;i++){ // target bin loop
+				g_target_br_ex[p][i] = new TGraph(REx_bin[p][i],REx[p][i],RBr[p][i]);
 				os.str("");
 				os << "g_" << name << "_br_ex_" << p << "_" << i;
-				g_target_br_ex[p][i] = new TGraph;
 				g_target_br_ex[p][i]->SetName(os.str().c_str());
 				g_target_br_ex[p][i]->SetMarkerStyle(7);
 				g_target_br_ex[p][i]->SetMarkerColor(color_root[p]);
 				g_target_br_ex[p][i]->SetLineWidth(2);
 				g_target_br_ex[p][i]->SetLineColor(color_root[p]);
-				index_br_ex[p][i]=0;
-			}
-		}
-
-		float max_total_pop[parity]={0};
-		for(int i=0;i<bin_target;i++){ // target bin loop
-			float Ex_target = nuc_target->Ex[0][i];
-			float pop_target_sum = nuc_target->GetPopParitySum(i);
-
-			for(int par=0;par<parity;par++){
-				float pop_target_norm = nuc_target->pop[par][i]/nuc_target->total_pop[par];
-				// fill normalized population
-				g_target_pop[par]->SetPoint(index[par],Ex_target,pop_target_norm);
-				if(max_total_pop[par]<pop_target_norm) max_total_pop[par] = pop_target_norm;
-				index[par]++;
-				
-			}
-			
-			// calculate br
-			for(int p=0;p<num_particle;p++){ // particle loop
-				if((!nuc_target->flag_decay_data[i] && i<=20)){ 
-					if(p==0){
-						g_target_br[p]->SetPoint(index_br[p],Ex_target,1);
-						index_br[p]++;
-						// Fill level info only for gamma
-						for(int li=0;li<=i;li++){
-							g_target_br_ex[p][i]->SetPoint(index_br_ex[p][i],	
-																						 nuc_target->level_Ex[li],
-																						 nuc_target->level_br[i][li]*1e-2); // % to frac
-							index_br_ex[p][i]++;
-						}
-					}else{
-						g_target_br[p]->SetPoint(index_br[p],Ex_target,0);
-						index_br[p]++;
-					}
-				}else if(pop_target_sum>0 && nuc_target->flag_decay_data[i]){ // have continuous data
-					float population = nuc_target->GetPopDaughterBinSum(p,i);//  (particle, exbin)
-					float br = population/pop_target_sum;
-					if(br<=0) br=0;
-					if(br>=1) br=1;
-					g_target_br[p]->SetPoint(index_br[p],Ex_target,br);
-					index_br[p]++;
-					for(int j=0;j<nuc_target->Ex_bin_p[p][i] && population>0;j++){ // daughter ex bin loop
-						float br_ex = nuc_target->pop_p[p][i][j]/population;
-						if(br_ex<=0) br_ex=0;
-						if(br_ex>=1) br_ex=1;
-						g_target_br_ex[p][i]->SetPoint(index_br_ex[p][i],
-																					 nuc_target->Ex_p[p][i][j],br_ex);
-						index_br_ex[p][i]++;
-					}
-				}
 			}
 		}
 
