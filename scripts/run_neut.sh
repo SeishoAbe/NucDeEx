17c9c305fefc4be169b8b240f849b26e6d50b355
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-07-25 05:06:57 +0000

    prepare directory for shell scripts

---
+++ scripts/run_neut.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+prefix_tmp="neut_1GeV_numu_CCQE_C_MDLQE422 neut_1GeV_numub_CCQE_C_MDLQE422 neut_1GeV_numu_CCQE_O_MDLQE422_NUCDEXITE0 neut_1GeV_numub_CCQE_O_MDLQE422_NUCDEXITE0"
+
+Exe=./bin/neut
+LogDir=./log_neut
+
+for prefix in $prefix_tmp
+do
+	LogFile=$LogDir/log_$prefix
+	$Exe $prefix > $LogFile 2>&1 &
+done 
