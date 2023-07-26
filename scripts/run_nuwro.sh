b1c8601912173ea8c9a49caa9cf98c05577e0846
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2023-07-26 06:15:06 +0000

    add pinit plot. add genie

---
+++ scripts/run_nuwro.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+prefix_tmp="SF_LFG_-14_1000_CCQE_C SF_LFG_-14_1000_CCQE_O SF_LFG_14_1000_CCQE_C SF_LFG_14_1000_CCQE_O"
+
+Exe=./bin/nuwro
+LogDir=./log_nuwro
+
+for prefix in $prefix_tmp
+do
+	LogFile=$LogDir/log_$prefix
+	(time -f "CPU time %E: Memory %M KB" $Exe $prefix) > $LogFile 2>&1 &
+done 
