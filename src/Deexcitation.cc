39d04f7ff342d28a91935b2dcd588aeb1e1562da
seisho <seisho@km.icrr.u-tokyo.ac.jp>
2024-01-10 14:24:49 +0900

    add some initialization. avoid singed <-> unsigned comparison

--- src/Deexcitation.cc
+++ src/Deexcitation.cc
@@ -525,7 +525,7 @@ int Deexcitation::DecayMode(const double Ex)
 
 	double Br_integ=0;
 	double random = rndm->Rndm();
-	int decay_mode;
+	int decay_mode=-1;
 	for(int p=0;p<NucDeEx::num_particle;p++){
 		Br[p] /= Br_sum; // Normalize Br just in case.  
 		Br_integ += Br[p];
@@ -557,7 +557,10 @@ int Deexcitation::DecayMode(const double Ex)
 	}else if(decay_mode==6){
 		Z_daughter-=2;
 		N_daughter-=2;
-	}
+	}else{
+    std::cerr << "Unexpected decay_mode = " << decay_mode << std::endl;
+    exit(1);
+  }
 	nuc_daughter = _nucleus_table->GetNucleusPtr(Z_daughter,N_daughter);
 	if(nuc_daughter!=NULL) name_daughter = nuc_daughter->name;
 	else{
