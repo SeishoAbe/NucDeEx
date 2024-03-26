efdde5e34876fd440e0067e83c73f6a2029f961a
seisho <seisho@km.icrr.u-tokyo.ac.jp>
2024-03-26 17:36:40 +0900

    change initial value of NucDeExEventInfo::fStatus (0->1)

--- src/NucDeExEventInfo.cc
+++ src/NucDeExEventInfo.cc
@@ -12,7 +12,7 @@
 #include "NucDeExEventInfo.hh"
 
 /////////////////////////////////////////////
-NucDeExEventInfo::NucDeExEventInfo(): EventID(0), fStatus(0), fShell(0),
+NucDeExEventInfo::NucDeExEventInfo(): EventID(0), fStatus(1), fShell(0),
                                       Zt(0), Nt(0), Z(0), N(0), Ex(0)
 /////////////////////////////////////////////
 {
@@ -30,7 +30,8 @@ NucDeExEventInfo::~NucDeExEventInfo()
 void NucDeExEventInfo::InitParameters()
 /////////////////////////////////////////////
 {
-  EventID = fStatus = fShell =0;
+  EventID = fShell =0;
+  fStatus = 1;
   Zt = Nt = Z = N = 0;
   Ex = 0.;
   ParticleVector.clear();
