33243dc048ce6a5cfae90302ed3ba0774d963da6
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2024-03-27 16:43:14 +0900

    cout random seed

--- src/NucDeExRandom.cc
+++ src/NucDeExRandom.cc
@@ -1,9 +1,14 @@
+#include <iostream>
 #include "NucDeExRandom.hh"
 
 namespace NucDeEx{
   namespace Random{
     TRandom3 rndm(1);
-    double random(){ return rndm.Rndm(); };
-    void SetSeed(int s){ rndm.SetSeed(s); };
+    double random(){ return rndm.Rndm(); }
+    void SetSeed(int s)
+    { 
+      rndm.SetSeed(s);
+      std::cout << "NucDeEx::Random::SetSeed(" << s << ")" << std::endl;
+    }
   }
 }
