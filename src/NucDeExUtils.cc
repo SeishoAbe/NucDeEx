4780b6194b0274f6abb7f8a8fe13a0e1a8e16ca5
SeishoAbe <seisho@km.icrr.u-tokyo.ac.jp>
2026-06-26 15:32:56 +0900

    test

--- src/NucDeExUtils.cc
+++ src/NucDeExUtils.cc
@@ -28,6 +28,7 @@ namespace NucDeEx{
         fTGeoManager = new TGeoManager("NucDeEx","NucDeEx");
         fTGeoElementTable = fTGeoManager->GetElementTable();
         gGeoManager = old_global_manager;
+        std::Cout << "TGeoManager new" << std::endl;
       }
     }
 
